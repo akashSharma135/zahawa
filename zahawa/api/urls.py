deebc22fe4193926c5a72b96e1f58325d0961a7c
pythexcel <pythexcel@excellencetechnologies.in>
2021-09-06 09:43:11 +0200

    ZHW-11-Fav-completed

--- zahawa/api/urls.py
+++ zahawa/api/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("event_types", views.EventsTypeView.as_view()),
+    path("event_types/", views.EventsTypeView.as_view()),
     path("room/", views.RoomView.as_view()),
     path("Vendor/", views.VendorListView.as_view()),
     # path("Vendor/service/", views.VendorServiceView.as_view()),
